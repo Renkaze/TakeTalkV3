26e7afb35309e5f1e4ec142c511166555f5981d7
FranckyF <foretf@essilor.fr>
2016-12-28 09:29:21 +0100

    simplification UI & add Zone text report meeting

--- .meteor/local/build/programs/server/app/server/methods.js
+++ .meteor/local/build/programs/server/app/server/methods.js
@@ -2,7 +2,7 @@
  * This methods alows us to send an e-mail to someone
  * @param {string} to - The receiver's e-mail
  * @param {string} from - The sender's e-mail
- * @param {string} subject - The e-mail's header 
+ * @param {string} subject - The e-mail's header
  * @param {string} text - The e-mail's body
 */
 Meteor.methods({
@@ -14,14 +14,15 @@ Meteor.methods({
   },
 
   //Permet de réinitiliser la base de données
-  resetAll: function() {
-      Session.set("meetingId", "");
-      Session.set("userId", "");
-      Speeches.remove({});
-      Users.remove({});
-      Meetings.remove({});
-  }
+  // resetAll: function() {
+  //     Session.set("meetingId", "");
+  //     Session.set("userId", "");
+  //     Speeches.remove({});
+  //     Users.remove({});
+  //     Meetings.remove({});
+  // }
 });
+
 }).call(this);
 
 //# sourceMappingURL=methods.js.map
