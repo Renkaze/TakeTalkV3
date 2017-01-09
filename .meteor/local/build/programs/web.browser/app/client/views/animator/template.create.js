b9d9b5f8c77d221b7a82053aff43fc5b90477722
Thomas Berriau <thomas.berriau@etu.u-pec.fr>
2017-01-09 09:50:25 +0100

    fin internationalisation

--- .meteor/local/build/programs/web.browser/app/client/views/animator/template.create.js
+++ .meteor/local/build/programs/web.browser/app/client/views/animator/template.create.js
@@ -2,7 +2,11 @@
 Template.__checkName("create");
 Template["create"] = new Template("Template.create", (function() {
   var view = this;
-  return [ HTML.Raw('<div class="page-head">\n    <h2>Create a meeting</h2>\n</div>\n'), HTML.DIV({
+  return [ HTML.DIV({
+    class: "page-head"
+  }, "\n    ", HTML.H2(Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createTitle");
+  })), "\n"), "\n", HTML.DIV({
     class: "main-content"
   }, "\n    ", HTML.DIV({
     class: "row"
@@ -10,14 +14,24 @@ Template["create"] = new Template("Template.create", (function() {
     class: "col-md-8 col-md-offset-2"
   }, "\n            ", HTML.DIV({
     class: "panel panel-default"
-  }, "\n                ", HTML.Raw('<div class="panel-heading"><span class="title">Create a meeting</span></div>'), "\n                ", HTML.DIV({
+  }, "\n                ", HTML.DIV({
+    class: "panel-heading"
+  }, HTML.SPAN({
+    class: "title"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createTitle");
+  }))), "\n                ", HTML.DIV({
     class: "panel-body"
   }, "\n                    ", HTML.FORM({
     role: "form",
     id: "create-form"
   }, "\n\n                        ", HTML.DIV({
     class: "form-group"
-  }, "\n                          ", HTML.Raw('<label for="animatorName">Your name *</label>'), "\n                          ", HTML.INPUT({
+  }, "\n                          ", HTML.LABEL({
+    for: "animatorName"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createAnimatorName");
+  })), "\n                          ", HTML.INPUT({
     id: "animatorName",
     name: "animatorName",
     type: "text",
@@ -29,7 +43,11 @@ Template["create"] = new Template("Template.create", (function() {
     autofocus: ""
   }), "\n                        "), "\n\n                        ", HTML.DIV({
     class: "form-group"
-  }, "\n                            ", HTML.Raw('<label for="animatorEmail">Your e-mail *</label>'), "\n                            ", HTML.DIV({
+  }, "\n                            ", HTML.LABEL({
+    for: "animatorEmail"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createMail");
+  })), "\n                            ", HTML.DIV({
     class: "input-group"
   }, HTML.Raw('<span class="input-group-addon">@</span>'), "\n                                ", HTML.INPUT({
     id: "animatorEmail",
@@ -41,9 +59,23 @@ Template["create"] = new Template("Template.create", (function() {
       return Spacebars.mustache(view.lookup("authorAdress"));
     },
     required: ""
-  }), "\n                            "), "\n                        "), "\n\n                        ", HTML.Raw('<div class="form-group">\n                            <label for="meetingName">Your meeting\'s name *</label>\n                            <input id="meetingName" name="meetingName" type="text" class="form-control" required="">\n                        </div>'), "\n\n                        ", HTML.Raw('<div class="form-group">\n                            <label for="reportLink">Link of the report</label>\n                            <input id="reportLink" name="reportLink" type="url" class="form-control">\n                        </div>'), "\n\n                        ", HTML.Raw('<div class="form-group">\n                            <label>Agenda</label>\n                            <div class="agenda-group" rank="1">\n                                <div class="row">\n                                    <div class="col-sm-9 xs-mb-15"><div class="input-group">\n                                        <span class="input-group-addon"><i class="glyphicon glyphicon-list-alt"></i></span>\n                                        <input class="agenda-name-input form-control" name="ordreDuJour" type="text">\n                                    </div></div>\n                                    <div class="col-sm-3 xs-mb-15"><div class="input-group">\n                                        <input min="1" parsley-type="number" class="agenda-time-input form-control" name="ordreDuJourTemps" type="number">\n                                        <span class="input-group-addon">min</span>\n                                    </div></div>\n                                </div>\n                            </div>\n                        </div>'), "\n\n                        ", HTML.DIV({
+  }), "\n                            "), "\n                        "), "\n\n                        ", HTML.DIV({
+    class: "form-group"
+  }, "\n                            ", HTML.LABEL({
+    for: "meetingName"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createMeetingName");
+  })), "\n                            ", HTML.Raw('<input id="meetingName" name="meetingName" type="text" class="form-control" required="">'), "\n                        "), "\n\n                        ", HTML.DIV({
+    class: "form-group"
+  }, "\n                            ", HTML.LABEL({
+    for: "reportLink"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createLink");
+  })), "\n                            ", HTML.Raw('<input id="reportLink" name="reportLink" type="url" class="form-control">'), "\n                        "), "\n\n                        ", HTML.Raw('<div class="form-group">\n                            <label>Agenda</label>\n                            <div class="agenda-group" rank="1">\n                                <div class="row">\n                                    <div class="col-sm-9 xs-mb-15"><div class="input-group">\n                                        <span class="input-group-addon"><i class="glyphicon glyphicon-list-alt"></i></span>\n                                        <input class="agenda-name-input form-control" name="ordreDuJour" type="text">\n                                    </div></div>\n                                    <div class="col-sm-3 xs-mb-15"><div class="input-group">\n                                        <input min="1" parsley-type="number" class="agenda-time-input form-control" name="ordreDuJourTemps" type="number">\n                                        <span class="input-group-addon">min</span>\n                                    </div></div>\n                                </div>\n                            </div>\n                        </div>'), "\n\n                        ", HTML.DIV({
     class: "form-group participant-email-group"
-  }, "\n                            ", HTML.Raw("<label>Your participants' e-mails</label>"), "\n\n                            ", Blaze.Each(function() {
+  }, "\n                            ", HTML.LABEL(Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createParticipantsMail");
+  })), "\n\n                            ", Blaze.Each(function() {
     return Spacebars.call(view.lookup("members"));
   }, function() {
     return [ "\n\n                              ", HTML.DIV({
@@ -69,7 +101,14 @@ Template["create"] = new Template("Template.create", (function() {
     rank: function() {
       return Spacebars.mustache(Spacebars.dot(view.lookup("members"), "length"));
     }
-  }, "\n                                ", HTML.Raw('<div class="input-group xs-mb-15"><span class="input-group-addon">@</span>\n                                    <input class="participant-email-input form-control" name="participantsEmails" type="email" parsley-type="email">\n                                </div>'), "\n                            "), "\n                        "), "\n                        ", HTML.Raw('<div class="spacer text-right">\n                            <button type="submit" class="btn btn-space btn-primary">Create your meeting</button>\n                        </div>'), "\n                    "), "\n                "), "\n            "), "\n        "), "\n    "), "\n") ];
+  }, "\n                                ", HTML.Raw('<div class="input-group xs-mb-15"><span class="input-group-addon">@</span>\n                                    <input class="participant-email-input form-control" name="participantsEmails" type="email" parsley-type="email">\n                                </div>'), "\n                            "), "\n                        "), "\n                        ", HTML.DIV({
+    class: "spacer text-right"
+  }, "\n                            ", HTML.BUTTON({
+    type: "submit",
+    class: "btn btn-space btn-primary"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "createMeeting");
+  })), "\n                        "), "\n                    "), "\n                "), "\n            "), "\n        "), "\n    "), "\n") ];
 }));
 
 }).call(this);
--- .meteor/local/build/programs/web.browser/app/client/views/application/template.meeting.js
+++ .meteor/local/build/programs/web.browser/app/client/views/application/template.meeting.js
@@ -25,7 +25,9 @@ Template["meeting"] = new Template("Template.meeting", (function() {
       },
       title: "Collaborative document",
       target: "_blank"
-    }, "Link of the report")), "\n                            "), "\n                        " ];
+    }, Blaze.View("lookup:_", function() {
+      return Spacebars.mustache(view.lookup("_"), "meetingLink");
+    }))), "\n                            "), "\n                        " ];
   }), "\n                        ", HTML.Raw("<!-- Liste des sujets � l'ordre du jour -->"), "\n                        ", Blaze.Each(function() {
     return Spacebars.call(view.lookup("ordres"));
   }, function() {
@@ -44,7 +46,13 @@ Template["meeting"] = new Template("Template.meeting", (function() {
     class: "col-md-4"
   }, "\n            ", HTML.DIV({
     class: "panel panel-default panel-heading-fullwidth panel-primary"
-  }, "\n                ", HTML.Raw('<div class="panel-heading"><span class="title">People lining up</span></div>'), "\n                ", HTML.DIV({
+  }, "\n                ", HTML.DIV({
+    class: "panel-heading"
+  }, HTML.SPAN({
+    class: "title"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingLineUp");
+  }))), "\n                ", HTML.DIV({
     class: "panel-body"
   }, "\n                    ", HTML.DIV({
     class: "row"
@@ -131,7 +139,9 @@ Template["meeting"] = new Template("Template.meeting", (function() {
     }), "\n                                              ", Blaze.If(function() {
       return Spacebars.dataMustache(view.lookup("isSubject"), view.lookup("_id"));
     }, function() {
-      return [ "\n                                                  ", HTML.BR(), "Keywords: ", Blaze.View("lookup:subject", function() {
+      return [ "\n                                                  ", HTML.BR(), Blaze.View("lookup:_", function() {
+        return Spacebars.mustache(view.lookup("_"), "meetingKeywords");
+      }), " ", Blaze.View("lookup:subject", function() {
         return Spacebars.mustache(view.lookup("subject"));
       }), "\n                                              " ];
     }), "\n                                          "), "\n                                          ", Blaze.If(function() {
@@ -156,7 +166,13 @@ Template["meeting"] = new Template("Template.meeting", (function() {
     class: "col-md-4"
   }, "\n            ", HTML.DIV({
     class: "panel panel-default panel-heading-fullwidth panel-primary"
-  }, "\n                ", HTML.Raw('<div class="panel-heading"><span class="title">People on this meeting</span></div>'), "\n                    ", HTML.DIV({
+  }, "\n                ", HTML.DIV({
+    class: "panel-heading"
+  }, HTML.SPAN({
+    class: "title"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingMembers");
+  }))), "\n                    ", HTML.DIV({
     class: "panel-body"
   }, "\n                        ", HTML.DIV({
     class: "row"
@@ -215,18 +231,134 @@ Template["meeting"] = new Template("Template.meeting", (function() {
       class: "btn btn-primary",
       "data-toggle": "modal",
       "data-target": "#localModal"
-    }, "Add local participant"), "\n                        ", HTML.BUTTON({
+    }, Blaze.View("lookup:_", function() {
+      return Spacebars.mustache(view.lookup("_"), "meetingAdd");
+    })), "\n                        ", HTML.BUTTON({
       type: "button",
       class: "btn btn-primary",
       "data-toggle": "modal",
       "data-target": "#invitationModal"
-    }, "Invite participants"), "\n                        ", HTML.BUTTON({
+    }, Blaze.View("lookup:_", function() {
+      return Spacebars.mustache(view.lookup("_"), "meetingInvite");
+    })), "\n                        ", HTML.BUTTON({
       type: "button",
       class: "btn btn-danger",
       role: "button",
       id: "closeMeeting"
-    }, "Close meeting"), "\n                    "), "\n                "), "\n            "), "\n        "), "\n    "), "\n    " ];
-  }), "\n"), HTML.Raw('\n\n<!-- ************************************ -->\n<!-- Modal d\'ajout d\'un participant local -->\n<!-- ************************************ -->\n<div class="modal fade" id="localModal" tabindex="-1" role="dialog" aria-labelledby="localModalLabel">\n    <div class="modal-dialog" role="document">\n        <div class="modal-content">\n            <div class="modal-header">\n                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\n                <h4 class="modal-title" id="localModalLabel">New Local Participant</h4>\n            </div>\n            <div class="modal-body">\n                <form role="form" id="localForm">\n                    <div class="form-group name-input-group">\n                        <label class="control-label">Participants\' Name</label>\n                        <div class="participantNameInput" rank="1">\n                            <div class="xs-mb-15">\n                                <input type="text" class="participantsName form-control" name="participantsName">\n                            </div>\n                        </div>\n                    </div>\n                    <div class="modal-footer">\n                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\n                        <button id="localSubmit" type="submit" class="btn btn-primary">Add</button>\n                    </div>\n                </form>\n            </div>\n        </div>\n    </div>\n</div>\n\n<!-- **************************************** -->\n<!-- Modal d\'ajout d\'un participant ext�rieur -->\n<!-- **************************************** -->\n<div class="modal fade" id="invitationModal" tabindex="-1" role="dialog" aria-labelledby="invitationModalLabel">\n    <div class="modal-dialog" role="document">\n        <div class="modal-content">\n            <div class="modal-header">\n                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\n                <h4 class="modal-title" id="invitationModalLabel">Invite Participants</h4>\n            </div>\n            <div class="modal-body">\n                <!--\n                <h3>By QR Code</h3>\n                <div id="qrcode"></div>\n                 -->\n                <h3>By email address</h3>\n                <form role="form" id="inviteForm">\n                    <div class="form-group email-input-group">\n                        <label class="control-label">Your participants\' e-mails</label>\n                        <div class="participantEmailInput" rank="1">\n                            <div class="input-group xs-mb-15"><span class="input-group-addon">@</span>\n                                <input class="participantsEmails form-control" name="participantsEmails" type="email" parsley-type="email">\n                            </div>\n                        </div>\n                    </div>\n                    <div class="modal-footer">\n                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>\n                        <button id="inviteSubmit" type="submit" class="btn btn-primary">Invite</button>\n                    </div>\n                </form>\n            </div>\n        </div>\n    </div>\n</div>\n\n<!-- ************************************************************************************************************* -->\n<!-- Modal de notification qui s\'affiche sur le dashboard d\'un intervenant auquel l\'animateur a supprim� le speech -->\n<!-- ************************************************************************************************************* -->\n<div class="modal fade" id="speech-delete-modal" tabindex="-1" role="dialog" aria-labelledby="speech-delete-label">\n    <div class="modal-dialog" role="document">\n        <div class="modal-content">\n            <div class="modal-header">\n                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\n                <h4 class="modal-title" id="speech-delete-label">Invite Participants</h4>\n            </div>\n            <div class="modal-body">\n                Your speech have been deleted\n            </div>\n        </div>\n    </div>\n</div>') ];
+    }, Blaze.View("lookup:_", function() {
+      return Spacebars.mustache(view.lookup("_"), "meetingClose");
+    })), "\n                    "), "\n                "), "\n            "), "\n        "), "\n    "), "\n    " ];
+  }), "\n"), HTML.Raw("\n\n<!-- ************************************ -->\n<!-- Modal d'ajout d'un participant local -->\n<!-- ************************************ -->\n"), HTML.DIV({
+    class: "modal fade",
+    id: "localModal",
+    tabindex: "-1",
+    role: "dialog",
+    "aria-labelledby": "localModalLabel"
+  }, "\n    ", HTML.DIV({
+    class: "modal-dialog",
+    role: "document"
+  }, "\n        ", HTML.DIV({
+    class: "modal-content"
+  }, "\n            ", HTML.DIV({
+    class: "modal-header"
+  }, "\n                ", HTML.Raw('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'), "\n                ", HTML.H4({
+    class: "modal-title",
+    id: "localModalLabel"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingNewParticipant");
+  })), "\n            "), "\n            ", HTML.DIV({
+    class: "modal-body"
+  }, "\n                ", HTML.FORM({
+    role: "form",
+    id: "localForm"
+  }, "\n                    ", HTML.DIV({
+    class: "form-group name-input-group"
+  }, "\n                        ", HTML.LABEL({
+    class: "control-label"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingParticipantsName");
+  })), "\n                        ", HTML.Raw('<div class="participantNameInput" rank="1">\n                            <div class="xs-mb-15">\n                                <input type="text" class="participantsName form-control" name="participantsName">\n                            </div>\n                        </div>'), "\n                    "), "\n                    ", HTML.DIV({
+    class: "modal-footer"
+  }, "\n                        ", HTML.BUTTON({
+    type: "button",
+    class: "btn btn-default",
+    "data-dismiss": "modal"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "close");
+  })), "\n                        ", HTML.BUTTON({
+    id: "localSubmit",
+    type: "submit",
+    class: "btn btn-primary"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "add");
+  })), "\n                    "), "\n                "), "\n            "), "\n        "), "\n    "), "\n"), HTML.Raw("\n\n<!-- **************************************** -->\n<!-- Modal d'ajout d'un participant ext�rieur -->\n<!-- **************************************** -->\n"), HTML.DIV({
+    class: "modal fade",
+    id: "invitationModal",
+    tabindex: "-1",
+    role: "dialog",
+    "aria-labelledby": "invitationModalLabel"
+  }, "\n    ", HTML.DIV({
+    class: "modal-dialog",
+    role: "document"
+  }, "\n        ", HTML.DIV({
+    class: "modal-content"
+  }, "\n            ", HTML.DIV({
+    class: "modal-header"
+  }, "\n                ", HTML.Raw('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'), "\n                ", HTML.H4({
+    class: "modal-title",
+    id: "invitationModalLabel"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingInvite");
+  })), "\n            "), "\n            ", HTML.DIV({
+    class: "modal-body"
+  }, "\n                ", HTML.Raw('<!--\n                <h3>By QR Code</h3>\n                <div id="qrcode"></div>\n                 -->'), "\n                ", HTML.H3(Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingByMail");
+  })), "\n                ", HTML.FORM({
+    role: "form",
+    id: "inviteForm"
+  }, "\n                    ", HTML.DIV({
+    class: "form-group email-input-group"
+  }, "\n                        ", HTML.LABEL({
+    class: "control-label"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingParticipantsMail");
+  })), "\n                        ", HTML.Raw('<div class="participantEmailInput" rank="1">\n                            <div class="input-group xs-mb-15"><span class="input-group-addon">@</span>\n                                <input class="participantsEmails form-control" name="participantsEmails" type="email" parsley-type="email">\n                            </div>\n                        </div>'), "\n                    "), "\n                    ", HTML.DIV({
+    class: "modal-footer"
+  }, "\n                        ", HTML.BUTTON({
+    type: "button",
+    class: "btn btn-default",
+    "data-dismiss": "modal"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "close");
+  })), "\n                        ", HTML.BUTTON({
+    id: "inviteSubmit",
+    type: "submit",
+    class: "btn btn-primary"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "invite");
+  })), "\n                    "), "\n                "), "\n            "), "\n        "), "\n    "), "\n"), HTML.Raw("\n\n<!-- ************************************************************************************************************* -->\n<!-- Modal de notification qui s'affiche sur le dashboard d'un intervenant auquel l'animateur a supprim� le speech -->\n<!-- ************************************************************************************************************* -->\n"), HTML.DIV({
+    class: "modal fade",
+    id: "speech-delete-modal",
+    tabindex: "-1",
+    role: "dialog",
+    "aria-labelledby": "speech-delete-label"
+  }, "\n    ", HTML.DIV({
+    class: "modal-dialog",
+    role: "document"
+  }, "\n        ", HTML.DIV({
+    class: "modal-content"
+  }, "\n            ", HTML.DIV({
+    class: "modal-header"
+  }, "\n                ", HTML.Raw('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'), "\n                ", HTML.H4({
+    class: "modal-title",
+    id: "speech-delete-label"
+  }, Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingInvite");
+  }), " "), "\n            "), "\n            ", HTML.DIV({
+    class: "modal-body"
+  }, "\n                ", Blaze.View("lookup:_", function() {
+    return Spacebars.mustache(view.lookup("_"), "meetingSpeechDeleted");
+  }), "\n            "), "\n        "), "\n    "), "\n") ];
 }));
 
 Template.__checkName("ordre");
@@ -241,9 +373,13 @@ Template["ordre"] = new Template("Template.ordre", (function() {
   }, function() {
     return [ Blaze.View("lookup:time", function() {
       return Spacebars.mustache(view.lookup("time"));
-    }), " min estimated" ];
+    }), " ", Blaze.View("lookup:_", function() {
+      return Spacebars.mustache(view.lookup("_"), "meetingMinEstimated");
+    }) ];
   }, function() {
-    return " no duration scheduled";
+    return [ " ", Blaze.View("lookup:_", function() {
+      return Spacebars.mustache(view.lookup("_"), "meetingNoDuration");
+    }) ];
   }));
 }));
 
